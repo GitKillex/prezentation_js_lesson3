0f87f4d5d904d98641d70c70cfcbd853223c72e7
Macbook <macbook@MacBook-Pro-Macbook.local>
2023-05-16 23:35:00 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,60 @@
+// function (){
+//     let str = "abubakr"
+//     let str1 = str[0].toUpperCase() + str.slice(1);
+// }
+// console.log(str1)
+
+// task 1
+// function addSomething(str){
+//     let something = "something";
+//     return something.concat(str)
+// }
+// console.log(addSomething("boolewevfvdd"))
+
+// task 2
+// function toString(str){
+//     return str.toString()
+// }
+// console.log(toString(true))
+
+// task 3
+// function toInt(str){
+//     return Number(str)
+// }
+// console.log(toInt("6"))
+
+// task 4
+// function myLove(str){
+//     const name = "Mubashir";
+//     if(str == name){
+//         return `Hello, my Love!`
+//     }
+//     else{
+//         return `Hello, ${str}!`
+//     }
+// }
+// console.log(myLove("Matt"))
+// console.log(myLove("Patt"))
+// console.log(myLove("Mubashir"))
+
+// task 5
+// function comp(x,y){
+//     if(x.length == y.length){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+// console.log(comp('abf','fg'))
+
+// task 6
+// function comp(x,y){
+//     if(x.length == y.length){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+// console.log(comp('abf','fg'))
---
+++ readme.md
@@ -0,0 +1,1 @@
+<h1 align="center">text</h1># prezentation_js_lesson3
